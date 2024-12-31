99c9d020d16d6ced12398ef949566c61d62093b2
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2024-12-31 15:40:16 +0100

    Update start.sh

--- proxy/start.sh
+++ proxy/start.sh
@@ -10,8 +10,8 @@ DNS_PORT=${DNS_PORT:-53}
 LISTEN_ADDRESS=${LISTEN_ADDRESS:-127.0.0.1}
 
 # Delay proxy start by 30 minutes (1800 seconds)
-# echo "Delaying proxy start by 30 minutes..."
-# sleep 120
+echo "Delaying proxy start by 2 minutes..."
+sleep 120
 
 # Generate redsocks.conf dynamically
 cat <<EOF > /etc/redsocks.conf
@@ -84,7 +84,7 @@ ip6tables -A OUTPUT -j DROP
 # Create dnsmasq configuration
 cat <<EOF > /etc/dnsmasq.conf
 no-resolv
-server=${LISTEN_ADDRESS}#10053  # Route DNS to redsocks UDP port
+server=${LISTEN_ADDRESS}#10053
 listen-address=${LISTEN_ADDRESS}
 EOF
 
@@ -95,6 +95,7 @@ EOF
 
 # Start dnsmasq in the background
 dnsmasq -k &
+sleep 2
 
 # Signal that redsocks is ready
 echo "redsocks ready" > /tmp/redsocks_ready
