baeabd7f5cf121d7838387b7db0e3d2dfee2b45e
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-04 13:02:41 +0100

    dev

--- proxy/start.sh
+++ proxy/start.sh
@@ -9,9 +9,10 @@ DNS_PORT=${DNS_PORT:-53}
 
 LISTEN_ADDRESS=${LISTEN_ADDRESS:-127.0.0.1}
 
-# Delay proxy start by 30 minutes (1800 seconds)
-echo "Delaying proxy start by 30 minutes..."
-sleep 1800
+DELAY=${DELAY:-1800}
+
+echo "Delaying proxy start by ${DELAY} seconds..."
+sleep "${DELAY}"
 
 # Generate redsocks.conf dynamically
 cat <<EOF > /etc/redsocks.conf
