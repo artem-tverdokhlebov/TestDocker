db27b2f50cbf685ea03ef7a226aaa6c07872f8d6
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-04 19:23:27 +0100

    Update start.sh

--- proxy/start.sh
+++ proxy/start.sh
@@ -64,11 +64,17 @@ iptables -A OUTPUT -p udp --dport 5353 -j ACCEPT
 # Allow multicast traffic (224.0.0.0/4)
 iptables -A OUTPUT -p udp -d 224.0.0.0/4 -j ACCEPT
 
-# Block STUN/TURN servers
+# Allow traffic to local networks (loopback, private IP ranges)
+iptables -A OUTPUT -p udp -d 127.0.0.0/8 -j ACCEPT
+iptables -A OUTPUT -p udp -d 10.0.0.0/8 -j ACCEPT
+iptables -A OUTPUT -p udp -d 172.16.0.0/12 -j ACCEPT
+iptables -A OUTPUT -p udp -d 192.168.0.0/16 -j ACCEPT
+
+# Block STUN/TURN servers (common internet UDP services)
 iptables -A OUTPUT -p udp --dport 3478:3479 -j DROP
 
-# Block general UDP traffic in the dynamic port range
-iptables -A OUTPUT -p udp --dport 49152:65535 -j DROP
+# Block all other UDP traffic to the internet (dynamic port range and beyond)
+iptables -A OUTPUT -p udp -d 0.0.0.0/0 --dport 49152:65535 -j DROP
 
 # DNS
 
