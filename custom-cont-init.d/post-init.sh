7875226f30ec1e2c93b9c959938d8752ce13d9ed
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-14 23:28:34 +0100

    Update post-init.sh

--- custom-cont-init.d/post-init.sh
+++ custom-cont-init.d/post-init.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+sleep 10
+
+# Apply iptables rules for WireGuard routing
+iptables -t nat -A POSTROUTING -s 172.20.0.0/24 -o wg0 -j MASQUERADE
+iptables -A FORWARD -i wg0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth0 -o wg0 -j ACCEPT
+
+# Log success
+echo "iptables rules applied successfully"
