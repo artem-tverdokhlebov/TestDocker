881880e6d8555ae907895a00683246027742e4ee
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-14 23:02:09 +0100

    Update post-init.sh

--- custom-cont-init.d/post-init.sh
+++ custom-cont-init.d/post-init.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-# Function to check if the WireGuard interface is active
-wait_for_wireguard() {
-    local interface="wg0"
-    local retries=10
-    local wait_time=2
-
-    echo "Waiting for WireGuard interface ($interface) to initialize..."
-    while [ $retries -gt 0 ]; do
-        if ip link show "$interface" | grep -q "state UP"; then
-            echo "WireGuard interface ($interface) is up!"
-            return 0
-        fi
-        echo "WireGuard interface ($interface) not ready. Retrying in $wait_time seconds..."
-        sleep $wait_time
-        retries=$((retries - 1))
-    done
-
-    echo "Error: WireGuard interface ($interface) did not come up in time."
-    exit 1
-}
-
-# Wait for WireGuard to initialize
-wait_for_wireguard
-
 # Enable IP forwarding
 sysctl -w net.ipv4.ip_forward=1
 
