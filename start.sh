7a20c2d08e138ebb4412008e3c208a94639694ad
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-15 22:06:47 +0100

    dev

--- start.sh
+++ start.sh
@@ -7,15 +7,4 @@ sudo docker compose -p macos_project down
 sudo docker system prune -f
 
 # Start and rebuild the container
-sudo docker compose -p macos_project up --build -d
-
-# Wait for the VNC port to be available
-VNC_PORT=5999
-echo "Waiting for VNC server to be ready on localhost:$VNC_PORT..."
-while ! nc -z localhost $VNC_PORT; do
-    sleep 1
-done
-echo "VNC server is now ready!"
-
-# Launch VNC viewer
-vncviewer localhost:$VNC_PORT
+sudo docker compose -p macos_project up --build
