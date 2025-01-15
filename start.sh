a64772c7509f542bb46544632c5c45b4811ea5a3
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-15 22:14:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,26 @@ sudo docker compose -p macos_project down
 sudo docker system prune -f
 
 # Start and rebuild the container
-sudo docker compose -p macos_project up --build
+sudo docker compose -p macos_project up --build -d
+
+VNC_PORT=5999
+VNC_HOST=localhost
+
+echo "Checking if VNC server is fully operational on $VNC_HOST:$VNC_PORT..."
+
+while true; do
+    # Try connecting to the VNC server and read the response
+    RESPONSE=$(echo -n | nc $VNC_HOST $VNC_PORT)
+
+    # Check if the response contains the expected VNC handshake (RFB protocol)
+    if [[ $RESPONSE == RFB* ]]; then
+        echo "VNC server is fully operational! Handshake response: $RESPONSE"
+        break
+    else
+        echo "VNC server not ready yet. Retrying in 1 second..."
+        sleep 1
+    fi
+done
+
+echo "VNC server is ready. Launching VNC viewer..."
+vncviewer $VNC_HOST:$VNC_PORT
