c1789033bdb9416988744362c8ac3535980c9466
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-16 13:31:21 +0100

    Update configurable-startup.sh

--- configurable-startup.sh
+++ configurable-startup.sh
@@ -13,7 +13,7 @@ if [ "$DELAY" -gt 0 ]; then
         SECONDS=$((DELAY % 60))
         
         # Display the countdown timer
-        printf "\r[DELAY] \033[32mTime remaining: %02d:%02d:%02d\033[0m" "$HOURS" "$MINUTES" "$SECONDS"
+        printf "\r\n[DELAY] \033[32mTime remaining: %02d:%02d:%02d\033[0m" "$HOURS" "$MINUTES" "$SECONDS"
         
         # Wait for 1 second
         sleep 1
