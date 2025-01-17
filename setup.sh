8b9a79e66856024459c38f3e85d1b996523d4d5e
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-17 22:08:14 +0100

    d

--- setup.sh
+++ setup.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-# Function to clean up background processes and Docker containers
-cleanup() {
-    echo -e "\r\033[33mCleaning up...\033[0m"
-
-    echo -e "\r\033[33mStopping Docker container...\033[0m"
-    docker compose -p osxvm down
-    echo -e "\r\033[33mCleanup complete. Exiting.\033[0m"
-
-    exit 0
-}
-
-# Trap Ctrl+C (SIGINT) and call the cleanup function
-trap cleanup SIGINT SIGTERM SIGHUP
-
 # Stop existing containers
 docker compose -p osxvm down
 
 # Start the containers in detached mode
 echo -e "\r\033[33mStarting Docker containers in detached mode...\033[0m"
 DELAY=1000 docker compose -p osxvm up --build
-
-# Clean up after VNC viewer is closed
-cleanup
