b7e8817fdf10d5c1678fb4aa0f70be3c81f5614c
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-17 21:45:32 +0100

    d

--- continue.sh
+++ continue.sh
@@ -5,7 +5,7 @@ cleanup() {
     echo -e "\r\033[33mCleaning up...\033[0m"
 
     echo -e "\r\033[33mStopping Docker container...\033[0m"
-    sudo docker compose -p osxvm down
+    docker compose -p osxvm down
     echo -e "\r\033[33mCleanup complete. Exiting.\033[0m"
 
     exit 0
@@ -15,13 +15,11 @@ cleanup() {
 trap cleanup SIGINT SIGTERM SIGHUP
 
 # Stop and remove existing containers
-sudo docker compose -p osxvm down
+docker compose -p osxvm down
 
 # Start the containers in detached mode
 echo -e "\r\033[33mStarting Docker containers in detached mode...\033[0m"
-
-export DELAY=0
-sudo docker compose -p osxvm up 
+DELAY=0 docker compose -p osxvm up 
 
 # Clean up after VNC viewer is closed
 cleanup
