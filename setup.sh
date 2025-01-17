b5f9aa05175770eda11edb1b7b447e4dce04945e
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-17 21:41:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,11 @@ sudo docker compose -p osxvm down
 
 # Start the containers in detached mode
 echo -e "\r\033[33mStarting Docker containers in detached mode...\033[0m"
-DELAY=1000 sudo docker compose -p osxvm up --build
+
+DELAY=1000
+export DELAY
+
+sudo docker compose -p osxvm up --build
 
 # Clean up after VNC viewer is closed
 cleanup
