3671dea23f4d52f8b7bfee972d3deda612cd4361
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-20 15:02:33 +0100

    remove proxy

--- continue.sh
+++ continue.sh
@@ -6,4 +6,4 @@ docker compose -p osxvm down
 
 # Start the containers in detached mode
 echo -e "\r\033[33mStarting Docker containers in detached mode...\033[0m"
-DELAY=0 docker compose -p osxvm up
+docker compose -p osxvm up
