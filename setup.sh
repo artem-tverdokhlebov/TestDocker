c5c6fe0088aec8f81223584c6b975dc3e18b39e4
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-21 16:19:15 +0100

    remove project name

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 # Stop existing containers
 echo -e "\r\033[33mStopping existing Docker containers...\033[0m"
-docker compose -p osxvm down
+docker compose down
 
 # Start the containers in detached mode
 echo -e "\r\033[33mStarting Docker containers in detached mode...\033[0m"
-docker compose -p osxvm up --build
+docker compose up --build
