d41ba12b105f8ff118c164431500e15a49ebcacb
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-27 16:56:53 +0100

    name

--- reset.sh
+++ reset.sh
@@ -2,7 +2,7 @@
 
 # Stop existing containers
 echo -e "\r\033[33mStopping existing Docker containers...\033[0m"
-docker compose -p osxvm down
+docker compose down
 
 # Remove data directory contents
 echo -e "\r\033[31mRemoving data directory contents...\033[0m"
--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 # Stop existing containers
 echo -e "\r\033[33mStopping existing Docker containers...\033[0m"
-docker compose -p osxvm down
+docker compose down
 
 # Start the containers in detached mode
 echo -e "\r\033[33mStarting Docker containers in detached mode...\033[0m"
-DELAY=1000 docker compose -p osxvm up --build
+DELAY=1000 docker compose up --build
